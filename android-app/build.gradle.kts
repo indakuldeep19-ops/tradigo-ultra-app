ca68e9b7669fba4a257f357967ccd2443bb6334b
indakuldeep19-ops <indakuldeep19@gmail.com>
2026-06-06 18:32:35 +0530

    Create build.gradle.kts

---
+++ android-app/build.gradle.kts
@@ -0,0 +1,13 @@
+// Top-level build file
+plugins {
+    id("com.android.application") version "8.2.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    id("com.google.gms.google-services") version "4.4.0" apply false
+    id("com.google.dagger.hilt.android") version "2.50" apply false
+}
+
+buildscript {
+    dependencies {
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.50")
+    }
+}
